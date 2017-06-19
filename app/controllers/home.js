87a0ce734df0165e5fab598bb1e6249d524b71c8
Atul MalaviyaAtul Malaviya <atulmal@microsoft.com>
2017-06-19 19:14:39 +0530

    Added API action

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -13,3 +13,25 @@ router.get('/', function (req, res, next) {
       articles: articles
     });
 });
+
+router.get('/users', function (req, res, next) {
+  var myObj = {
+    "App":"Fabrikam API",
+    "Department":"Express",
+    "users": [
+      {"name":"boring_dubinsky", "age":31, "city":"New York"},
+      {"name":"elegant_franklin", "age":30, "city":"London"},
+      {"name":"cocky_khorana", "age":28, "city":"Paris"},
+      {"name":"romantic_northcutt", "age":30, "city":"Tokyo"},
+      {"name":"lonely_fermat", "age":42, "city":"New York"},
+      {"name":"jovial_booth", "age":41, "city":"Dallas"},
+      {"name":"elated_yonath", "age":39, "city":"Seattle"},
+      {"name":"suspicious_goldberg", "age":22, "city":"Hyderabad"},
+      {"name":"reverent_raman", "age":24, "city":"New York"},
+      {"name":"agitated_borg", "age":19, "city":"Paris"},
+      {"name":"determined_kilby", "age":21, "city":"London"}
+    ]
+ }
+
+  res.send(JSON.stringify(myObj));
+});
