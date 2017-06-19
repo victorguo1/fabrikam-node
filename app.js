e600181c05e3da788b7a87e26e2c44880d121f0e
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2017-06-20 00:51:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ var app = express();
 module.exports = require('./config/express')(app, config);
 
 app.listen(config.port, function () {
-  console.log('Express Fabrikam web server listening on port ' + config.port);
+  console.log('Fabrikam web server listening on port ' + config.port);
 });
 
